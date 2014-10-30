509de537e76bcb6c793e78e5d40a0c1d69a30c1a
Mayron Cachina <mayroncachina@gmail.com>
2014-10-30 16:51:48 -0300

    cadastro de parcelas

--- app.js
+++ app.js
@@ -15,19 +15,30 @@ app.config(function($routeProvider,$httpProvider,$locationProvider){
 app.service('TitulosService', function(){
     
     this.salve = function(scope){
+      
+      var valor_parcela = scope.valor / scope.parcelas;
 
-      lib.insert("titulos", {
-        //estabelecimento: scope.estabelecimento.nome, 
-        descricao: scope.descricao, 
-        data: scope.date, 
-        parcelas: scope.parcelas, 
-        valor: scope.valor
-      });
+      for (var i = 0; i < scope.parcelas; i++) {
+
+        lib.insert("titulos", {
+          estabelecimento: scope.estabelecimento.name, 
+          descricao: scope.descricao, 
+          data: scope.date.setDate(scope.date.getDate()+30), 
+          parcela: i, 
+          valor: valor_parcela,
+          pago: false,
+          tipo: scope.tipo.name,
+
+        });
+      };
+      console.log(i)
       lib.commit();
-
-      console.log(lib.query("titulos"))
-
+      
       //return a.parcelas * a.valor;
+    }
+
+    this.ultimos90 = function(){
+      return lib.query("titulos")
     }
 
     this.add = function(a) { 
@@ -36,8 +47,9 @@ app.service('TitulosService', function(){
 });
 
 
-app.controller('indexCtrl', function($scope) {
+app.controller('indexCtrl', function($scope, TitulosService) {
    $scope.number = 0
+   $scope.titulos = TitulosService.ultimos90($scope);
 
 });
 
@@ -47,6 +59,13 @@ app.controller('newCtrl', function($scope, TitulosService) {
       {name:'CAIXA', vcto:'22', id:1},
       {name:'ITAU', vcto:'22', id:2},
     ];
+
+  $scope.tipos = [
+      {name:'Alimentacao', id:0},
+      {name:'Salario', id:1},
+      {name:'Aluguel', id:2},
+    ];
+
    
    $scope.gravar = function(){
       $scope.mensagem = "INSERIDO COM SUCESSO";
@@ -57,7 +76,7 @@ app.controller('newCtrl', function($scope, TitulosService) {
 
 
 if( lib.isNew() ) {
-    lib.createTable("titulos", ["estabelecimento", "descricao", "data", "parcelas", "valor"]);
+    lib.createTable("titulos", ["estabelecimento", "descricao", "data", "parcelas", "valor", "tipo", "pago"]);
     lib.insert("titulos", {estabelecimento: "0", descricao: "Teste", data: "20/10/2014", parcelas: 2, valor: 10});
     lib.commit();
 }
